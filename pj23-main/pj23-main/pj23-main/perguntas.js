d9670b371a512b389556bf7a7a9fb44bc4c6a0ec
ana16y <162391307+ana16y@users.noreply.github.com>
2024-12-03 22:29:22 -0300

    Add files via upload

---
+++ pj23-main/pj23-main/pj23-main/perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    "programção",
+    "O que e javaScript?",
+    "JavaScript é uma linguagem de programação"
+    )
+
+    criaCartao(
+        "geografia",
+        "qual a capital da frança?",
+        "a capital da frança e paris."
+        )
+
+        criaCartao(
+            categoria,
+            pergunta,
+            resposta
+            )
